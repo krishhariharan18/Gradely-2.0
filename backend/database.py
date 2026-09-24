e800740c557b694bbcaeed50dda824a54686586d
krishhariharan18 <krishchn.18@gmail.com>
2026-09-24 23:09:21 +0530

    Add SSL support for PlanetScale + cryptography dependency

--- backend/database.py
+++ backend/database.py
@@ -2,11 +2,13 @@ import pymysql
 from . import config
 
 def get_db_connection():
+    # ssl={"ssl": {}} enables SSL required by PlanetScale (and is harmless for other hosts)
     return pymysql.connect(
         host=config.DB_HOST,
         port=config.DB_PORT,
         user=config.DB_USER,
         password=config.DB_PASSWORD,
         database=config.DB_NAME,
+        ssl={"ssl": {}},
         cursorclass=pymysql.cursors.DictCursor
     )
